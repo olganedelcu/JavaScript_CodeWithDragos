1e487a0be62f084c99f556cbb0f72a100dfac951
olganedelcu <olganedelcuam@gmail.com>
2021-07-05 08:35:31 +0200

    Added first videos

---
+++ start/index.js
@@ -0,0 +1,7 @@
+function addChildren(){
+    const parent = document.getElementById("parent");
+    const newChildren = document.createElement("div");
+    newChildren.classList.add("children");
+
+    parent.appendChild(newChild);
+}
